f3151cdc80704777e0bc1c94e6c9960e1438e33c
TinyMarsh <ranismyth@gmail.com>
2017-01-19 11:29:43 +0000

    added comments

--- converter.cpp
+++ converter.cpp
@@ -27,16 +27,19 @@ int main(){
 	str+=(directory.length()+1);
 	filePaths.push_back(directory);
 
+	//Iterate through the char pointer containing the file paths and store the paths in a vector
 	while(*str){
 		std::string filename = str;
 		filePaths.push_back(filename);
 		str+=(filename.length()+1);
 	}
 
+	//We want to set the current directory to the Mascot cgi folder so that the export_data_2.pl script runs correctly
 	std::string mascotDir = "D:\\inetpub\\mascot\\cgi\\";
 	SetCurrentDirectory(mascotDir.c_str());
 
-	for(int i=0, dirSize=filePaths[0].size(); i<dirSize; i++){
+	//The export_dat_2.pl script seems to only like double forward slashes in the DAT file paths. So we replace every backslash and insert 2 forward slashes
+	for(int i=0; i<filePaths[0].size(); i++){
 		if(filePaths[0][i]=='\\'){
 			filePaths[0][i]='/';
 			filePaths[0].insert(i,"/");
