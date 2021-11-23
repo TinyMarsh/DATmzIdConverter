2aac403c0c8da0a2ca25bbc5774552c4c857ef0a
unknown <ranismyth@gmail.com>
2021-11-23 18:33:17 +0000

    creating pepxml converter

--- converter.cpp
+++ converter.cpp
@@ -47,10 +47,10 @@ int main(){
 	}
 
 	for(int i=1, numFiles=filePaths.size(); i<numFiles; i++){
-		std::string systemString = "C:\\inetpub\\mascot\\perl64\\bin\\perl.exe export_dat_2.pl _ignoreionsscorebelow=0.1 _minpeplen=0 _server_mudpit_switch=99999999 _showsubsets=1 _sigthreshold=1 do_export=1 export_format=mzIdentML file=";
+		std::string systemString = "C:\\inetpub\\mascot\\perl64\\bin\\perl.exe export_dat_2.pl _ignoreionsscorebelow=0.1 _minpeplen=0 _server_mudpit_switch=99999999 _showsubsets=1 _sigthreshold=1 do_export=1 export_format=pepXML file=";
 		systemString+=filePaths[0]+"//"+filePaths[i]+" group_family=0 pep_exp_mz=1 pep_isbold=1 pep_isunique=1 pep_query=1 pep_rank=1 peptide_master=1 prot_acc=1 prot_desc=1 prot_hit_num=1 report=1000000 sessionid=all_secdisabledsession show_pep_dupes=1 show_same_sets=1 > ";
 		std::string mzidFile = filePaths[i].erase(filePaths[i].length()-3);
-		mzidFile+=std::string("mzid");
+		mzidFile+=std::string("pepXML");
 		systemString+=filePaths[0]+"//"+mzidFile;
 		std::cout << "\rConverting DAT file " << i << " / " << numFiles-1;
 		system(systemString.c_str());
